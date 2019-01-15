4cc3d172367fe5a8e7dc424bc5b9b55f1059af10
Rimma <rim23ma@gmail.com>
2019-01-15 11:24:30 -0600

    third step

--- bash.js
+++ bash.js
@@ -1,14 +1,15 @@
 process.stdout.write('prompt > ');
-
+const pwd = require('./pwd')
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd());
-  }
-  else{
-    process.stdout.write('You typed: ' + cmd);
-  }
+  pwd(cmd)
+  // if(cmd === 'pwd'){
+  //   process.stdout.write(process.cwd());
+  // }
+  // else{
+  //   process.stdout.write('You typed: ' + cmd);
+  // }
 
-  process.stdout.write('\nprompt > ');
+  // process.stdout.write('\nprompt > ');
 
 })
---
+++ pwd.js
@@ -0,0 +1,11 @@
+module.exports = (cmd) => {
+  // process.stdin.on('data', (data) => {
+  //   const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    process.stdout.write(process.cwd());
+  }
+  else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+  process.stdout.write('\nprompt > ');
+}
