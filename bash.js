5928d91877b84d091544fc91281a2ca4e8631832
Rimma <rim23ma@gmail.com>
2019-01-15 11:56:53 -0600

    almost

--- bash.js
+++ bash.js
@@ -1,13 +1,24 @@
 process.stdout.write('prompt > ');
 const pwd = require('./pwd')
 const ls = require('./ls')
+const fs = require('fs')
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
+  const cmdArray = cmd.split(' ');
   if(cmd === 'pwd'){
     pwd();
   }
   else if(cmd === 'ls'){
     ls();
+  }
+  else if (cmdArray[0] === 'cat') {
+    fs.readFile(`./${cmdArray[1]}`, 'utf8', function (err, data) {
+      if (err) {
+        throw err
+      }
+      process.stdout.write(data);
+      process.stdout.write('\nprompt > ');
+    })
   }
   else{
     process.stdout.write('You typed: ' + cmd);
